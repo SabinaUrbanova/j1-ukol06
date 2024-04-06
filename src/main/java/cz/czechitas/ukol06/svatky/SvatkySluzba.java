8e1841aad76cf8346f9c73862effcb39e3637195
Sabina Urbanova <urbanova.sabina@gmail.com>
2024-04-06 12:28:13 +0200

    6. ukol

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class SvatkySluzba {
-
     private final ObjectMapper objectMapper = JsonMapper.builder()
             .addModule(new JavaTimeModule())
             .build();
